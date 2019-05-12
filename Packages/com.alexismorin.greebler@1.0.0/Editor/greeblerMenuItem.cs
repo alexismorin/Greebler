ae65b78541345aa0d0b47a09af94c72fc53d0b49
Alexis <alexis.morin.mail@gmail.com>
2019-05-11 21:00:48 -0400

    you can now spawn multiple greeblers

--- Packages/com.alexismorin.greebler@1.0.0/Editor/greeblerMenuItem.cs
+++ Packages/com.alexismorin.greebler@1.0.0/Editor/greeblerMenuItem.cs
@@ -7,10 +7,6 @@ public class greeblerMenuItem : MonoBehaviour {
 
     [MenuItem ("Tools/Spawn Greebler")]
     static void spawnGreebler () {
-        if (GameObject.Find ("Greebler") != null) {
-            DestroyImmediate (GameObject.Find ("Greebler"));
-        }
-
         GameObject greeblerManager = new GameObject ("Greebler");
         greeblerManager.AddComponent<BoxCollider> ();
         greeblerManager.GetComponent<BoxCollider> ().size = new Vector3 (10f, 10f, 10f);
