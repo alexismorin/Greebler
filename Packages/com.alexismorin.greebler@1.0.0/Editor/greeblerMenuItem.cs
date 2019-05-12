6f2028dfcb460a4b967741cb5161ad9c4965e26f
Alexis <alexis.morin.mail@gmail.com>
2019-05-11 21:02:09 -0400

    raycast fix

--- Packages/com.alexismorin.greebler@1.0.0/Editor/greeblerMenuItem.cs
+++ Packages/com.alexismorin.greebler@1.0.0/Editor/greeblerMenuItem.cs
@@ -8,7 +8,9 @@ public class greeblerMenuItem : MonoBehaviour {
     [MenuItem ("Tools/Spawn Greebler")]
     static void spawnGreebler () {
         GameObject greeblerManager = new GameObject ("Greebler");
+        greeblerManager.layer = Physics.IgnoreRaycastLayer;
         greeblerManager.AddComponent<BoxCollider> ();
+        greeblerManager.AddComponent<BoxCollider> ().isTrigger;
         greeblerManager.GetComponent<BoxCollider> ().size = new Vector3 (10f, 10f, 10f);
         greeblerManager.AddComponent<greebler> ();
     }
